6779f4fc5dbe08cff1dfbcad6621d1eedeb70ec6
sinaetown <ksw08215@gmail.com>
2023-11-16 15:11:47 +0900

    alter query addeed

--- basic/1.DDl.sql
+++ basic/1.DDl.sql
@@ -5,6 +5,7 @@ CREATE DATABASE board;
 USE board;
 
 -- --------------------------------------------
+
 -- author 테이블 신규 생성
 CREATE TABLE author(id INT, name VARCHAR(255), 
 email VARCHAR(255), password VARCHAR(255), 
@@ -15,7 +16,9 @@ SHOW TABLES;
 
 -- 테이블 COLUMN 조회
 DESCRIBE author;
+
 -- --------------------------------------------
+
 -- post 테이블 신규 생성
 CREATE TABLE posts(id INT PRIMARY KEY, 
 title VARCHAR(255), content VARCHAR(255), 
@@ -26,6 +29,8 @@ SHOW TABLES;
 
 -- 테이블 COLUMN 조회
 DESCRIBE posts;
+
+-- --------------------------------------------
 
 -- 테이블 COLUMN 상세 조회
 SHOW FULL COLUMNS FROM author;
@@ -45,3 +50,29 @@ SHOW CREATE TABLE posts;
 -- 테이블 제약 조건 조회 (USE 안 하고 사용 가능)
 SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
 WHERE TABLE_NAME = 'posts';
+
+-- 테이블 인덱스 조회
+SHOW INDEX FROM posts;
+
+-- --------------------------------------------
+
+-- ALTER문
+-- 테이블 이름 변경 -> ex) posts를 post로 
+ALTER TABLE 테이블명 RENAME 새로운_테이블명;
+-- ex) ALTER TABLE posts RENAME post;
+
+-- 칼럼 추가 (ADD) -> role이라는 칼럼 추가
+ALTER TABLE 테이블명 ADD COLUMN 칼럼명 타입 [제약 조건];
+-- ex) ALTER TABLE author ADD COLUMN role VARCHAR(50);
+
+-- 필드 타입 변경 (MODIFY) -> name 필드의 VARCHAR의 100으로 변경
+ALTER TABLE 테이블명 MODIFY COLUMN 칼럼명 타입 [제약 조건];
+-- ex) ALTER TABLE post MODIFY COLUMN name VARCHAR(100) NOT NULL;
+
+-- 칼럼 이름 변경 -> content를 contents로
+ALTER TABLE 테이블명 CHANGE COLUMN 기존칼럼명 새로운칼럼명 타입 [제약조건];
+-- ALTER TABLE post CHANGE COLUMN content contents VARCHAR(255) NOT NULL;
+
+-- 칼럼 삭제 -> test1 칼럼 삭제
+ALTER TABLE 테이블명 DROP COLUMN 칼럼명;
+-- ALTER TABLE author DROP COLUMN test1;
