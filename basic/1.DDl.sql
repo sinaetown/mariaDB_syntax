7bfd64f56a7a0d20004503e8d9e48ba2c917843c
sinaetown <ksw08215@gmail.com>
2023-11-17 11:19:15 +0900

    more about select queries

--- basic/1.DDl.sql
+++ basic/1.DDl.sql
@@ -37,15 +37,7 @@ SHOW FULL COLUMNS FROM author;
 
 -- 테이블 생성문 조회
 SHOW CREATE TABLE posts;
--- CREATE TABLE `posts` (
---   `id` int(11) NOT NULL,
---   `title` varchar(255) DEFAULT NULL,
---   `content` varchar(255) DEFAULT NULL,
---   `author_id` int(11) DEFAULT NULL,
---   PRIMARY KEY (`id`),
---   KEY `author_id` (`author_id`),
---   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
 
 -- 테이블 제약 조건 조회 (USE 안 하고 사용 가능)
 SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
--- basic/3.DML.sql
+++ basic/3.DML.sql
@@ -12,6 +12,39 @@ SELECT 필드이름 FROM 테이블이름 WHERE 조건
 -- ex) SELECT * FROM author WHERE id>1;
 -- ex) SELECT * FROM author WHERE id>1 AND name='kim';
 
+-- 이름 중복 제거하고 조회
+SELECT DISTINCT 필드명 FROM 테이블명;
+-- ex) SELECT DISTINCT NAME FROM author;
+
+--정렬
+SELECT * FROM 테이블명 ORDER BY 필드명 [ASC/DESC]
+-- ex) SELECT * FROM author order by id desc;
+
+-- ORDER BY 멀티 : 먼저 쓴 column 우선 정렬 + 생략시 ASC 적용
+-- 여러 필드의 데이터를 쉼표 사용해서 여러 기준으로 정렬 가능
+-- ORDER BY 생략 시, PK 기준으로 오름차순 정렬하여 결과값 변환
+-- ex) SELECT * FROM author order by name desc, email asc;
+-- => name 우선 내림차순 정렬 + email 오름차순 정렬
+
+-- LIMIT 숫자 : 특정 숫자로 결과값 개수 확인
+SELECT * FROM AUTHOR LIMIT 숫자
+-- ex) SELECT * FROM author ORDER BY id DESC LIMIT 2
+
+
+-- ALIAS 별칭을 이용한 처리
+-- 테이블과 필드에 임시로 별칭 부여하고 해당 별칭을 SELECT문에서 상요
+SELECT 필드이름 AS 별칭 FROM 테이블이름;
+SELECT 필드이름 FROM 테이블이름 AS 별칭;
+
+-- ALIAS 이용한 SELECT문
+-- AS는 생략 가능
+-- ex) SELECT NAME AS 이름 FROM author;
+-- ex) SELECT NAME 이름, email FROM author;
+
+-- ex) SELECT NAME, email FROM author AS a; 
+-- => 다른 테이블인데 같은 이름을 가진 column 구별할 때 사용
+-- 두 테이블이 관련되어 있을 때 헷갈리지 않도록
+
 -- UPDATE
 -- 테이블 레코드의 내용을 수정
 UPDATE 테이블이름 SET 필드이름1=데이터값, 필드이름2=데이터값2 ... WHERE 필드이름=데이터값;
