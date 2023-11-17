565c5ffdb3b314bd88509b74c630aa342e8749b2
sinaetown <ksw08215@gmail.com>
2023-11-17 16:28:50 +0900

    enum, datetime practice added

--- basic/4.DML_practice.sql
+++ basic/4.DML_practice.sql
@@ -72,7 +72,35 @@ insert into post(id, title, contents) values
 insert into post(id, title, contents) values
 (5, 'thank', 'you');
 
-
+-- 1 PRACTICE
 UPDATE POST SET AUTHOR_ID=NULL WHERE AUTHOR_ID IS NOT NULL;
 delete from author;
 select * from post order by title, contents desc limit 3;
+
+-- 2 PRACTICE
+ALTER TABLE AUTHOR ADD COLUMN AGE TINYINT UNSIGNED;
+SELECT * FROM AUTHOR;
+
+ALTER TABLE POST ADD COLUM PRICE DECIMAL(10,3);
+
+--3 ENUM PRACTICE
+-- role 타입을 enum타입으로 변경하고
+-- 'user', 'admin'으로 enum 타입 지정, not null로 설정,
+-- 입력이 없을 시에는 'user'로 세팅되도록 default 설정
+-- admin으로 데이터 세팅 후 insert
+-- super-user 데이터로 insert
+-- role 데이터 없이 insert
+update author set role='admin' where id=1 or id=3;
+insert into author(id, name, email, role) values (2, 'Drake', 'drake@gmail.com', 'user');
+
+insert into author(id, name, email) values (3, 'Jake', 'jake@gmail.com');
+alter table author modify column role enum('user', 'admin') not null default 'user';
+
+-- 4 DATETIME PRACTICE
+-- post 테이블에 DATETIME으로 created_time 칼럼 추가 및 default로 현재 시간 들어가도로 ㄱ설정
+-- datetime(6) default current_timestamp(6)
+-- 칼럼 추가 후 insert 테스트
+
+
+alter table post add column created_time DATETIME(6) default current_timestamp(6);
+insert into post(id, title, contents) values(6, 'hello', 'bye');
