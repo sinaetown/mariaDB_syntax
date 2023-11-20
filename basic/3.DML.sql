bc9b9d5c355499744842f79b1de829c69cc1b290
sinaetown <ksw08215@gmail.com>
2023-11-20 19:08:14 +0900

    transaction practice updated

--- basic/3.DML.sql
+++ basic/3.DML.sql
@@ -1,20 +1,28 @@
 -- INSERT, SELECT, UPDATE, DELETE
 
--- INSERT
+----------------------------------------
+
+INSERT
 -- 테이블에 새로운 레코드를 추가
 INSERT INTO 테이블이름(필드이름1, 필드이름2 ..) values (데이터값1, 데이터값2 ..);
 -- ex) INSERT INTO author(id, name, email) VALUES (1, 'kim', 'abc@naver.com');
 
--- SELECT
+---------------------------------------- 
+
+SELECT
 SELECT 필드이름 FROM 테이블이름 WHERE 조건
 -- ex) SELECT * FROM author;
 -- ex) SELECT name, email FROM author WHERE id=3;
 -- ex) SELECT * FROM author WHERE id>1;
 -- ex) SELECT * FROM author WHERE id>1 AND name='kim';
 
--- 이름 중복 제거하고 조회
+---------------------------------------- 
+
+이름 중복 제거하고 조회
 SELECT DISTINCT 필드명 FROM 테이블명;
 -- ex) SELECT DISTINCT NAME FROM author;
+
+---------------------------------------- 
 
 --정렬
 SELECT * FROM 테이블명 ORDER BY 필드명 [ASC/DESC]
@@ -30,9 +38,11 @@ SELECT * FROM 테이블명 ORDER BY 필드명 [ASC/DESC]
 SELECT * FROM AUTHOR LIMIT 숫자
 -- ex) SELECT * FROM author ORDER BY id DESC LIMIT 2
 
+---------------------------------------- 
 
--- ALIAS 별칭을 이용한 처리
--- 테이블과 필드에 임시로 별칭 부여하고 해당 별칭을 SELECT문에서 상요
+ALIAS (AS)
+-- 별칭을 이용한 처리
+-- 테이블과 필드에 임시로 별칭 부여하고 해당 별칭을 SELECT문에서 사용
 SELECT 필드이름 AS 별칭 FROM 테이블이름;
 SELECT 필드이름 FROM 테이블이름 AS 별칭;
 
@@ -42,10 +52,12 @@ SELECT 필드이름 FROM 테이블이름 AS 별칭;
 -- ex) SELECT NAME 이름, email FROM author;
 
 -- ex) SELECT NAME, email FROM author AS a; 
--- => 다른 테이블인데 같은 이름을 가진 column 구별할 때 사용
+-- 다른 테이블인데 같은 이름을 가진 column 구별할 때 사용
 -- 두 테이블이 관련되어 있을 때 헷갈리지 않도록
 
--- UPDATE
+---------------------------------------- 
+
+UPDATE
 -- 테이블 레코드의 내용을 수정
 UPDATE 테이블이름 SET 필드이름1=데이터값, 필드이름2=데이터값2 ... WHERE 필드이름=데이터값;
 -- WHERE절 생략 시, 해당 테이블의 모든 레코드값이 위 설정대로 수정되므로 유의
@@ -54,7 +66,9 @@ UPDATE 테이블이름 SET 필드이름1=데이터값, 필드이름2=데이터�
 -- WHERE 문 뒤에 나오는 비교문은 java기준으로 ==
 -- 어떤 값을 set할 때도 =을 사용하여 대입 (java =와 동일)
 
--- DELETE
+---------------------------------------- 
+
+DELETE
 -- 테이블에 데이터 삭제
 DELETE FROM 테이블이름 WHERE 필드이름=데이터값;
 -- ex) DELETE FROM author WHERE author_id=3;
--- basic/4.DML_practice.sql
+++ basic/4.DML_practice.sql
@@ -1,4 +1,6 @@
--- INSERT Practice
+--------------------------------------------------------------------------------
+
+-- INSERT PRACTICE
 insert into author(id, name, email) values 
 (1, 'kim', 'abc@naver.com');
 insert into author(id, name, email, password, role, address) values 
@@ -29,89 +31,78 @@ insert into post(id, title, contents, author_id) values
 
 select * from post;
 
--- UPDATE Practice
+--------------------------------------------------------------------------------
+
+-- UPDATE PRACTICE
 -- author 데이터 중 id가 4인 데이터를 email과 name을 변경
 UPDATE author set email='abc@naver.com', name='abc' where id=4;
 
--- Delete Practice
+-- DELETE PRACTICE
 -- post에 글쓴 적이 없는 author 데이터 1개 삭제
 delete from author where id=5; 
 
 -- post에 글쓴 적이 있는 author 데이터 1개 삭제 -> 에러 -> 조치 후 삭제
-
--- 연동이  되어있어서 에러남!
+-- 연동이 되어있어서 에러남!
 delete from author where id=3; 
 
 -- 조치방법 1.
 delete from post where author_id=3;
-delete from author where id=3; 
--- 2. or just make it as NULL
-update post set author_id=NULL where author_id=3;
+delete from author where id=3;
+
+-- 조치방법 2. Make it as NULL
+update post set author_id = NULL where author_id=3;
 delete from author where id=3; 
 
 -- author 삭제해도 글은 그대로 유지시키는 연습 
-insert into author(id, name, email) values 
-(1, 'kim', 'abc@naver.com');
-insert into author(id, name, email) values 
-(2, 'amy', 'amy@naver.com');
-insert into author(id, name, email) values 
-(3, 'bob', 'bob@google.com');
-insert into author(id, name, email) values 
-(4, 'chris', 'chris@google.com');
-insert into author(id, name, email) values 
-(5, 'david', 'david@yahoo.com');
-
-insert into post(id, title, contents, author_id) values
-(1, 'hello', 'world', 2);
-insert into post(id, title, contents, author_id) values
-(2, 'good', 'morning', 3);
-insert into post(id, title, contents) values
-(3, 'happy', 'holiday');
-insert into post(id, title, contents) values
-(4, 'merry', 'christmas');
-insert into post(id, title, contents) values
-(5, 'thank', 'you');
-
 -- 1 PRACTICE
-UPDATE POST SET AUTHOR_ID=NULL WHERE AUTHOR_ID IS NOT NULL;
-delete from author;
-select * from post order by title, contents desc limit 3;
+-- 조치방법 2 사용
+DELETE FROM author;
+UPDATE POST SET AUTHOR_ID = NULL WHERE AUTHOR_ID IS NOT NULL; 
+SELECT * FROM post order by title, contents desc limit 3;
 
--- 2 PRACTICE
+--------------------------------------------------------------------------------
+
+-- 숫자타입 
 ALTER TABLE AUTHOR ADD COLUMN AGE TINYINT UNSIGNED;
 SELECT * FROM AUTHOR;
-
 ALTER TABLE POST ADD COLUM PRICE DECIMAL(10,3);
 
---3 ENUM PRACTICE
--- role 타입을 enum타입으로 변경하고
--- 'user', 'admin'으로 enum 타입 지정, not null로 설정,
--- 입력이 없을 시에는 'user'로 세팅되도록 default 설정
--- admin으로 데이터 세팅 후 insert
--- super-user 데이터로 insert
--- role 데이터 없이 insert
-update author set role='admin' where id=1 or id=3;
-insert into author(id, name, email, role) values (2, 'Drake', 'drake@gmail.com', 'user');
+--------------------------------------------------------------------------------
 
-insert into author(id, name, email) values (3, 'Jake', 'jake@gmail.com');
+-- ENUM PRACTICE
+-- 1) role column의 타입을 ENUM 타입으로 변경
+-- 2) 'user', 'admin'으로 ENUM 타입 지정 & not null로 설정
+-- 3) 입력이 없을 시에는 'user'로 세팅되도록 default 설정
 alter table author modify column role enum('user', 'admin') not null default 'user';
 
--- 4 DATETIME PRACTICE
--- post 테이블에 DATETIME으로 created_time 칼럼 추가 및 default로 현재 시간 들어가도로 ㄱ설정
--- datetime(6) default current_timestamp(6)
+-- TRY1) admin으로 데이터 세팅 후 insert
+-- TRY2)super-user 데이터로 insert
+-- TRY3) role 데이터 없이 insert
+update author set role='admin' where id=1 or id=3;
+insert into author(id, name, email, role) values (2, 'Drake', 'drake@gmail.com', 'user');
+insert into author(id, name, email) values (3, 'Jake', 'jake@gmail.com');
+
+--------------------------------------------------------------------------------
+
+-- DATETIME PRACTICE
+-- post 테이블에 DATETIME으로 created_time 칼럼 추가 및 default로 현재 시간 들어가도록 설정
 -- 칼럼 추가 후 insert 테스트
 
 alter table post add column created_time DATETIME(6) default current_timestamp(6);
 insert into post(id, title, contents) values(6, 'hello', 'bye');
 
--- 5 Operator Practice
+--------------------------------------------------------------------------------
+
+-- OPERATOR PRACTICE
 select * from post where id not in (1,2,4);
 select * from post where id between 2 and 4;
 select * from post where id >= 2 AND id <=4;
 select * from post where id = 2 || id = 3 || id = 4;
 select * from post where not (id < 2 OR id > 4);
 
--- LIKE Practice
+--------------------------------------------------------------------------------
+
+-- LIKE PRACTICE
 select * from author where name like "%a%";
 select * from author where name like "%e";
 select * from author where name like "d%";
@@ -120,46 +111,94 @@ select * from author where name not like "%a%";
 select * from author where name not like "%e";
 select * from author where name not like "d%";
 
+--------------------------------------------------------------------------------
+-- CAST PRACTICE
 
--- CONSTARINT 
+SELECT CAST (20200101 AS DATE); 
+-- 2020-01-01로 출력
+
+SELECT CONVERT ('2020-01-01', DATE);
+-- 2020-01-01로 출력
+
+SELECT CONVERT ('2020-01-01', DATETIME);
+-- 2020-01-01 00:00:00으로 출력
+
+SELECT DATE_FORMAT('2020-01-01 15:12:30', '%Y-%m-%d');
+-- 2020-01-01로 출력
+
+SELECT * FROM author WHERE DATE_FORMAT(created_time, '%Y-%m-%d') = '2020-01-01';
+
+--Try) now()를 활용해서 오늘 날짜에 생성된 데이터 출력하기
+select * from post where date_format(created_time, "%Y-%m-%d") = date_format(now(),  "%Y-%m-%d");
+
+--------------------------------------------------------------------------------
+
+-- CONSTRAINT PRACTICE
 -- 제약 조건 걸면 인덱스 자동 생성
 -- 제약조건/인덱스 한 쪽만 삭제하면 다른 한 쪽도 자동 삭제
 
--- 방법 1.
--- 제약조건 걸기
-alter table author moidfy column email varchar(255) unique;
--- 제약조건 확인
-SELECT * FROM information_schema.table_constraints WHERE TABLE_NAME = 'author';
--- 인덱스 확인
-show index from author;
--- index 삭제
-alter table author drop index email; 
+NOT NULL PRACTICE
+--TRY) ALTER 문으로 post의 title를 NOT NULL 조건으로 바꾸기
+ALTER TABLE post MODIFY COLUMN title VARCHAR(255) not null;
 
--- 방법 2.
-alter table author add constraint author_unique unique(email);
--- 제약조건 확인
-SELECT * FROM information_schema.table_constraints WHERE TABLE_NAME = 'author';
--- 인덱스 확인
-show index from author;
+-- AUTO INCREMENT PRACTICE
+--TRY) author, post 테이블의 id에 AUTO_INCREMENT로 바꾸기
+ALTER TABLE post MODIFY COLUMN id INT AUTO_INCREMENT;
 
--- FOREIGN KEY ON DELETE / ON UPDATE
-foreign key(author_id) references author(id) on update cascade;
--- => 
+-- PRIMARY KEY PRACTICE
+...
+
+-- FOREIGN KEY PRACTICE
+--ex)
+ALTER TABLE post ADD CONSTRAINT
+post_author_fk FOREIGN KEY(author_id) REFERENCES author(id) ON UPDATE cascade;
+
+SELECT * FROM information_schema.key_column_usage WHERE table_name = 'post';
+ALTER TABLE post DROP FOREIGN KEY post_ibfk_1; --기존의 fk 삭제
+show index FROM post; --index 남아있어
+ALTER TABLE post DROP INDEX author_id; --index 삭제해주기
 
 
-----------------------------------------
-select * from information_schema.key_column_usage where table_name = 'post';
-alter table post drop foreign key post_ibfk_1; --기존의 fk 삭제
-show index from post; --index 남아있어
-alter table post drop index author_id; --index 삭제해주기
-alter table post add constraint post_author_fk foreign key(author_id) references author(id) on update cascade;
+-- 제약조건 걸기 modify나  add constraint 둘 중 하나 사용 가능
+ALTER TABLE post ADD CONSTRAINT
+post_author_fk FOREIGN KEY(author_id) REFERENCES author(id) ON UPDATE cascade;
+-- OR
+ALTER TABLE post ADD CONSTRAINT
+second_fk FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE SET NULL;
 
 -- author id를 update 했을 때 post에 author_id가 같이 바뀌었는지 확인
-update author set id = 10 where id = 1;
+UPDATE author SET id = 10 WHERE id = 1;
 
-----------------------------------------
+--------------------------------------------------------------------------------
+
+-- UNIQUE PRACTICE
+
+-- 방법 1 - MODIFY 사용.
+-- 제약조건 걸기
+ALTER TABLE author MODIFY COLUMN email VARCHAR(255) UNIQUE;
+-- 제약조건 확인
+SELECT * FROM information_schema.table_constraints WHERE TABLE_NAME = 'author';
+-- 인덱스 확인
+SHOW INDEX FROM author;
+-- index 삭제
+ALTER TABLE author DROP INDEX email; 
+
+-- 방법 2 - ADD CONSTRAINT 사용.
+ALTER TABLE author add constraint author_unique unique(email);
+-- 제약조건 확인
+SELECT * FROM information_schema.table_constraints WHERE TABLE_NAME = 'author';
+-- 인덱스 확인
+show index from author;
+
+-- DEFAULT PRACTICE
+
+ALTER TABLE author ADD create_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE post ADD create_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+
+--------------------------------------------------------------------------------
+
 -- CASE WHEN PRACTICE
--- ex) post테이블에서 id, title, contents, author_id의 경우, author_type이라는 이름으로 조회, author_id가 1인 경우 
+-- TRY) post테이블에서 id, title, contents, author_id의 경우, author_type이라는 이름으로 조회, author_id가 1인 경우 
 select id, title, contents, 
 case author_id
 when 1 then 'First Author'
@@ -168,7 +207,7 @@ else 'Others'
 end
 as author_type from post;
 
---ex) for checking null:
+-- TRY) for checking null:
 select id, title, contents, 
 case
 when author_id=1 then 'First Author'
@@ -178,17 +217,40 @@ else 'Others'
 end
 as author_type from post;
 
+--------------------------------------------------------------------------------
+
 -- IF Practice
--- ex) null이 아니면 author_id 출력, null이면 anonymous 출력
+-- TRY1) null이 아니면 author_id 출력, null이면 anonymous 출력
 select id, title, contents,ifnull(author_id, 'anonymous');
 
--- ex) if문 사용해서 만약 id가 1이면 first author, 그렇지 않으면 others
+-- TRY2) if문 사용해서 만약 id가 1이면 first author, 그렇지 않으면 others
 -- id, title, contents, author_type 순서대로 표현!
 select id, title, contents,
 if(author_id=1, 'first author', 'others') 
 as author_type from post;
 
--- ex) 위에서 사용한 ifnull의 결과값을 동일하게 if문으로 출력
+-- TRY3) TRY1에서 사용한 ifnull의 결과값을 동일하게 if문으로 출력
 select id, title, contents, 
 if (author_id is null, 'anonymous', author_id)
 as author_type from post;
+
+--------------------------------------------------------------------------------
+
+-- TRANSCATION PRACTICE
+
+-- ex1)
+insert into author(id, name, email) values(2, 'test', 'test@naver,com');
+insert into post(title, contents, author_id) values('hello', 'hello is', 10);
+-- - 위 코드 실행후 commit하면 첫번째 쿼리만 확정
+-- - 위 코드 실행후 rollback하면 모두 취소
+
+-- ex2)
+insert into author(id, name, email) values(1, 'test', 'test@naver,com');
+commit;
+insert into author(id, name, email) values(2, 'test2', 'test2@naver,com');
+insert into post(title, contents, author_id) values('hello', 'hello is', 10);
+-- - 첫번째 쿼리는 commit되어 확정
+-- - 두번째 쿼리는 실행은 되나 세번째 쿼리 실행이후 rollback을 하게 되면 함께 rollback되므로, 하나의 논리적인 transaction으로 볼 수 있음
+-- - 일반적으로는 사용자가 위와 같은 쿼리를 실행시킬수는 없으니, 프로그램에서 논리적인 트랜잭션을 지정하고, 전체 commit 또는 전체 rollback하는 명령을 실행
+
+rollback;
